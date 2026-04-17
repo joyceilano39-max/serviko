1a8bee530877659b6413e31ff7b4b2162754a688
Joyce Ilano <joyceilano40@gmail.com>
2026-04-17 15:31:59 +0800

    Add PWA icons

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+﻿import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.serviko.app",
+  appName: "Serviko",
+  webDir: "public",
+  server: {
+    url: "https://serviko.dev",
+    cleartext: true,
+  },
+};
+
+export default config;
