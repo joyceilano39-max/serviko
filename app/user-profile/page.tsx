2d11a723d34acf8721239ea5a76a4ccd9662941a
Joyce Ilano <joyceilano40@gmail.com>
2026-05-23 22:39:39 +0800

    Fix bugs: UTF-8 encoding, 404 error, profile routing

---
+++ app/user-profile/page.tsx
@@ -0,0 +1,9 @@
+﻿import { UserProfile } from "@clerk/nextjs";
+
+export default function UserProfilePage() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#f8f8f8", display: "flex", alignItems: "center", justifyContent: "center", padding: "24px" }}>
+      <UserProfile />
+    </div>
+  );
+}
